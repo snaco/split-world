d53066a905474d35a62795b67ff78da033a2f813
James Anderson <james@eaiti.com>
2022-08-21 21:07:40 -0400

    whoops

--- src/main/java/tech/snaco/SplitWorld/SplitWorld.java
+++ src/main/java/tech/snaco/SplitWorld/SplitWorld.java
@@ -1,35 +1,28 @@
 package tech.snaco.SplitWorld;
 
 import java.io.*;
-import java.util.List;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.GameMode;
-import tech.snaco.SplitWorld.mixin.SplitPlayer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.item.ItemStack;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class SplitWorld implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    Type ITEM_STACK_LIST_TYPE = new TypeToken<ArrayList<ItemStack>>() {}.getType();
+    Gson gson = new Gson();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
         ServerTickEvents.START_WORLD_TICK.register((world) -> {
             var players = world.getPlayers();
-            for (ServerPlayerEntity serverPlayer : players) {
-                var player = (SplitPlayer)serverPlayer;
+            for (var player : players) {
                 var pos = player.getPos();
                 if (pos.x < 0) {
                     setGameMode(player, GameMode.CREATIVE);
@@ -41,44 +34,40 @@ public class SplitWorld implements ModInitializer {
         });
 	}
 
-    private boolean setGameMode(SplitPlayer player, GameMode targetGameMode) {
-        // var savedCreative = getInventoryFile(player, GameMode.CREATIVE);
-        // var savedSurvival = getInventoryFile(player, GameMode.SURVIVAL);
-        // if (player.isCreative() && targetGameMode == GameMode.SURVIVAL) {
-        //     // save creative from player
-        //     saveInventoryFile(player.getInventory(), savedCreative);
-        //     // load survival
-        //     if (savedSurvival.exists()) {
-        //         player.getInventory().clone(loadInventoryFile(player, targetGameMode));
-        //     } else {
-        //         player.getInventory().clear();
-        //     }
-        // } else if (!player.isCreative() && targetGameMode == GameMode.CREATIVE) {
-        //     // save survival inventory
-        //     saveInventoryFile(player.getInventory(), savedSurvival);
-        //     // load creative inventory
-        //     if (savedCreative.exists()) {
-        //         player.getInventory().clone(loadInventoryFile(player, targetGameMode));
-        //     } else {
-        //         player.getInventory().clear();
-        //     }
-        // }
-        var backupInv = player.backupInventory;
-        player.backupInventory.clone(player.getInventory());
-        player.getInventory().clone(backupInv);
+    private boolean setGameMode(ServerPlayerEntity player, GameMode targetGameMode) {
+        var savedCreative = getInventoryFile(player, GameMode.CREATIVE);
+        var savedSurvival = getInventoryFile(player, GameMode.SURVIVAL);
+        if (player.isCreative() && targetGameMode == GameMode.SURVIVAL) {
+            // save creative from player
+            saveInventoryFile(player.getInventory(), savedCreative);
+            // load survival
+            if (savedSurvival.exists()) {
+                player.getInventory().clone(loadInventoryFile(player, targetGameMode));
+            } else {
+                player.getInventory().clear();
+            }
+        } else if (!player.isCreative() && targetGameMode == GameMode.CREATIVE) {
+            // save survival inventory
+            saveInventoryFile(player.getInventory(), savedSurvival);
+            // load creative inventory
+            if (savedCreative.exists()) {
+                player.getInventory().clone(loadInventoryFile(player, targetGameMode));
+            } else {
+                player.getInventory().clear();
+            }
+        }
         player.changeGameMode(targetGameMode);
         return true;
     }
 
-    private File getInventoryFile(SplitPlayer player, GameMode gameMode) {
+    private File getInventoryFile(ServerPlayerEntity player, GameMode gameMode) {
         var fileName = String.format("%s_%s.inv", player.getUuidAsString(), gameMode.toString());
         var file = new File(fileName);
         return file;
     }
 
-    private PlayerInventory loadInventoryFile(SplitPlayer player, GameMode gameMode) {
+    private PlayerInventory loadInventoryFile(ServerPlayerEntity player, GameMode gameMode) {
         var file = getInventoryFile(player, gameMode);
-        var gson = new Gson();
         try {
             var in = new FileInputStream(file);
             var br = new BufferedReader(new InputStreamReader(in));
@@ -90,7 +79,12 @@ public class SplitWorld implements ModInitializer {
             in.close();
             br.close();
             var contents = sb.toString();
-            return gson.fromJson(contents, PlayerInventory.class);
+            ArrayList<ItemStack> stacks = gson.fromJson(contents, ITEM_STACK_LIST_TYPE);
+            var temp_inv = new PlayerInventory(null);
+            for (int i = 0; i < temp_inv.size() && i < stacks.size(); i++) {
+                temp_inv.insertStack(i, stacks.get(i));
+            }
+            return temp_inv;
         } catch (Exception ex) {
             System.err.println("Error reading inventory file!");
             return null;
@@ -98,10 +92,10 @@ public class SplitWorld implements ModInitializer {
     }
 
     private void saveInventoryFile(PlayerInventory inventory, File file) {
-        var gson = new Gson();
         try {
+            var stacks = extractStacks(inventory);
             var out = new FileOutputStream(file);
-            var json = gson.toJson(inventory);
+            var json = gson.toJson(stacks);
             out.write(json.getBytes());
             out.close();
         } catch (Exception ex) {
@@ -109,4 +103,11 @@ public class SplitWorld implements ModInitializer {
         }
     }
 
+    private ArrayList<ItemStack> extractStacks(PlayerInventory inventory) {
+        var output = new ArrayList<ItemStack>();
+        for (int i = 0; i < inventory.size(); i++) {
+            output.add(inventory.getStack(i));
+        }
+        return output;
+    }
 }
