7be69f607012a7655fdae660fd1b6bd06cadba1d
snaco <jm.anderson.cs@gmail.com>
2022-08-26 02:25:13 -0400

    use gson instead of jackson

--- src/main/java/tech/snaco/SplitWorld/ConfigLoader.java
+++ src/main/java/tech/snaco/SplitWorld/ConfigLoader.java
@@ -1,37 +1,36 @@
 package tech.snaco.SplitWorld;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.slf4j.Logger;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class ConfigLoader {
   private static Logger LOGGER = SplitWorld.LOGGER;
-  private static File file = new File("config/splitworld.json");
-  private static ObjectMapper objectMapper = new ObjectMapper(new JsonFactory());
+  private static Path file = Path.of("config/splitworld.json");
+  private static Gson gson = new Gson();
 
   public static Config load() {
+    Config config;
     try {
-      var config = ConfigLoader.objectMapper.readValue(ConfigLoader.file, Config.class);
+      config = gson.fromJson(Files.readString(file), Config.class);
       LOGGER.info("Loaded config.");
-      return config;
+    } catch (JsonSyntaxException | IOException e) {
+      config = ConfigLoader.createConfig();
     }
-    catch (Exception ex) {
-      var config = ConfigLoader.createConfig();
-      LOGGER.info("Created new config.");
-      return config;
-    }
+    return config;
   }
-
+  
   private static Config createConfig() {
-    var config = new Config();
     try {
-      ConfigLoader.objectMapper.writeValue(ConfigLoader.file, config);
+      Files.writeString(file, gson.toJson(new Config()));
+      LOGGER.info("Created new config.");
     } catch (Exception ex) {
       LOGGER.error("Error writing config file!", ex);
     }
-    return config;
+    return new Config();
   }
 }
